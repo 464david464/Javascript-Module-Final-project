0998ce4b7b06903b7653ea51e68c27c98ba0b7e5
david weinstein <davidmenachem464@gmail.com>
2022-02-21 00:30:58 +0200

    done

--- customers.js
+++ customers.js
@@ -1,8 +1,9 @@
 function init() {
-  if(localStorage.getItem("isLog?") !== 'true') {
+  if (localStorage.getItem("isLog?") !== "true") {
     window.location.href = "index.html";
   }
 }
+
 function customersPage(cusEL) {
   const table = document.createElement("tr");
   table.classList.add("tr");
@@ -28,32 +29,101 @@ function customersPage(cusEL) {
   table.appendChild(lName);
   table.appendChild(email);
   table.appendChild(fullDetails);
+
+  table.addEventListener("click", function () {
+    let continerCustomer = document.querySelector("#cusomer");
+    continerCustomer.style.display = "flex";
+
+    document.querySelector(".continer").style.display = 'none';
+
+    let divCustomer = document.createElement("div");
+    divCustomer.classList.add("customerDtetailsWindow");
+
+    let customerId = document.createElement("p");
+    customerId.classList.add("customerId");
+
+    let customerName = document.createElement("p");
+    customerName.classList.add("name");
+
+    let imgCustomer = document.createElement("img");
+    let company = document.createElement("p");
+    let country = document.createElement("p");
+
+    let dateB = document.createElement("p");
+    customerName.classList.add("date");
+
+    let x = document.createElement("span");
+    x.innerText = "X";
+
+    continerCustomer.appendChild(divCustomer);
+    divCustomer.appendChild(customerId);
+    divCustomer.appendChild(customerName);
+    divCustomer.appendChild(imgCustomer);
+    divCustomer.appendChild(company);
+    divCustomer.appendChild(country);
+    divCustomer.appendChild(dateB);
+    divCustomer.appendChild(x);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open(
+      "GET",
+      `https://webschool-js-final-api.herokuapp.com/customers/${cusEL.id}`,
+      true
+    );
+
+    xhr.addEventListener("readystatechange", function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        const data = JSON.parse(xhr.responseText);
+
+        customerName.innerText = "name: " + data.first + " " + data.last;
+        imgCustomer.setAttribute("src", "https://picsum.photos/800/500");
+        dateB.innerText = "date of birth: " + data.created_at;
+        customerId.innerText = "customer id: " + data.id;
+        company.innerText = 'company: ' + data.company;
+        country.innerText = 'country: ' + data.country;
+
+       
+
+        
+        x.addEventListener("click", function () {
+          document.querySelector(".continer").classList.remove("blur");
+          continerCustomer.innerHTML = '';
+          document.querySelector(".continer").style.display = 'block'
+          continerCustomer.style.display = 'none'
+        });
+      }
+    });
+
+    xhr.send(null);
+  });
+
   return table;
 }
 
-init()
+init();
 // http request
 let xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://webschool-js-final-api.herokuapp.com/customers");
 if (xhr.readyState !== XMLHttpRequest.DONE) {
   let loadin = document.querySelector(".loadin");
-  let continer = document.querySelector('.continer');
+  let continer = document.querySelector(".continer");
 
-  loadin.style.display = 'flex';
-  continer.style.display = 'none'
-  loadin.style.backgroundImage = "url('images/Loader.gif')"
+  loadin.style.display = "flex";
+  continer.style.display = "none";
+  loadin.style.backgroundImage = "url('images/Loader.gif')";
 }
 xhr.addEventListener("readystatechange", function () {
   if (xhr.readyState === XMLHttpRequest.DONE) {
     const data = JSON.parse(xhr.responseText);
-    let loadinContiner = document.querySelector('#loadin')
+    let loadinContiner = document.querySelector("#loadin");
     let loadin = document.querySelector(".loadin");
-    let continer = document.querySelector('.continer');
+    let continer = document.querySelector(".continer");
 
-    loadinContiner.style.display = 'none'
-    loadin.style.display = 'none';
-    continer.style.display = 'block'
+    loadinContiner.style.display = "none";
+    loadin.style.display = "none";
+    continer.style.display = "block";
     for (let customers of data) {
       document.querySelector("tbody").appendChild(customersPage(customers));
     }
@@ -62,18 +132,18 @@ xhr.addEventListener("readystatechange", function () {
 
 xhr.send(null);
 
-const x = document.querySelector('#x');
-const logOut = document.querySelector('.log-out');
-const user = document.querySelector('.bi-person');
-const logOutBtn = document.querySelector('#btn-log-out');
+const x = document.querySelector("#x");
+const logOut = document.querySelector(".log-out");
+const user = document.querySelector(".bi-person");
+const logOutBtn = document.querySelector("#btn-log-out");
 
-user.addEventListener('click', function() {
-  logOut.style.display = 'flex'
-})
-x.addEventListener('click', function() {
-  logOut.style.display = 'none'
-})
-logOutBtn.addEventListener('click', function() {
-  localStorage.removeItem('isLog?');
+user.addEventListener("click", function () {
+  logOut.style.display = "flex";
+});
+x.addEventListener("click", function () {
+  logOut.style.display = "none";
+});
+logOutBtn.addEventListener("click", function () {
+  localStorage.removeItem("isLog?");
   location.reload();
-})
+});
--- main.js
+++ main.js
@@ -2,37 +2,53 @@
 function init() {
   if (localStorage.getItem("isLog?") == "true") {
     document.querySelector("#login").style.display = "none";
-    document.querySelector(".home-continer").style.display = 'flex';
-    document.body.style.backgroundColor = 'white';
+    document.querySelector(".home-continer").style.display = "flex";
+    document.body.style.backgroundColor = "white";
   }
 }
 
 function checkPass(pass) {
   const dataPass = {
-    pin: "4646",
+    pin: "4646"
   };
   return dataPass["pin"] === pass;
 }
+
 let numINp = document.querySelector(".num");
-let allBtn = document.querySelectorAll(".inp");
-let enter = document.querySelector("a");
+let allBtn = document.querySelectorAll(".numB");
+let enter = document.querySelector(".enter");
+let del = document.querySelector('.del');
 
+del.addEventListener("click", function () {
+  numINp.value = numINp.value.slice(0, -1);
+  numINp.removeAttribute("placeholder");
+})
 
-
-document.addEventListener("keyup", function (event) {
-  if (event.key != "Enter") {
-    for (let btns of allBtn) {
-      btns.addEventListener("click", function () {
-        numINp.value += btns.value;
-        length4();
-      });
-    }
+for (let btns of allBtn) {
+  btns.addEventListener("click", function () {
     if (numINp.value.length > 4) {
-      numINp.value = numINp.value.slice(0, -1);
-      document.querySelector(".moreFrome4").style.display = "block";
-    } else {
+      numINp.value.slice(0, -1);
+    }
+    if (numINp.value.length < 4) {
+      numINp.value += btns.value;
+    }
+  });
+}
+
+
+enter.addEventListener("click", login);
+
+document.addEventListener("keypress", function (event) {
+  if (event.key != "Enter") {
+    if (numINp.value.length > 4) {
+      numINp.value.slice(0, -1);
+    }
+    if (numINp.value.length < 4) {
       numINp.value += event.key;
-      document.querySelector(".moreFrome4").style.display = "none";
+      numINp.removeAttribute("placeholder");
+    }
+    if(event.keyCode == 8){
+      numINp.value = numINp.value.slice(0, -1);
       numINp.removeAttribute("placeholder");
     }
   } else {
@@ -40,54 +56,40 @@ document.addEventListener("keyup", function (event) {
   }
 });
 
-function length4() {
-  if (numINp.value.length > 4) {
-    numINp.value = numINp.value.slice(0, -1);
-    document.querySelector(".moreFrome4").style.display = "block";
-  }
-}
 
-allBtn[10].addEventListener("click", function () {
-  numINp.value = numINp.value.slice(0, -1);
-  document.querySelector(".moreFrome4").style.display = "none";
-  numINp.removeAttribute("placeholder");
-});
 
 function login() {
-  if(!checkPass(numINp.value)) {
-    document.querySelector(".moreFrome4").style.display = "none";
+  if (!checkPass(numINp.value)) {
+    numINp.value = "";
     numINp.setAttribute("placeholder", "The password is incorrect");
   } else {
     document.querySelector("#login").style.display = "none";
     localStorage.setItem("isLog?", "true");
-    document.querySelector(".home-continer").style.display = 'flex';
-    document.body.style.backgroundColor = 'white';
+    document.querySelector(".home-continer").style.display = "flex";
+    document.body.style.backgroundColor = "white";
   }
 }
 
 init();
-enter.addEventListener("click", login);
 
 //===========================================================
 
-
 // home Phge
-let productsBtn = document.querySelector('#products');
-let imgBgcDIv = document.querySelector('.ingView');
+let productsBtn = document.querySelector("#products");
+let imgBgcDIv = document.querySelector(".ingView");
 
-productsBtn.addEventListener('mouseenter', function(){
-    imgBgcDIv.style.backgroundImage = "url('images/product.jpg')"
+productsBtn.addEventListener("mouseenter", function () {
+  imgBgcDIv.style.backgroundImage = "url('images/product.jpg')";
 });
-productsBtn.addEventListener('mouseleave', function(){
-    imgBgcDIv.removeAttribute('style')
+productsBtn.addEventListener("mouseleave", function () {
+  imgBgcDIv.removeAttribute("style");
 });
 
+let customersBtn = document.querySelector("#customers");
 
-let customersBtn = document.querySelector('#customers');
-
-customersBtn.addEventListener('mouseenter', function(){
-    imgBgcDIv.style.backgroundImage = "url('images/customers.png')"
+customersBtn.addEventListener("mouseenter", function () {
+  imgBgcDIv.style.backgroundImage = "url('images/customers.png')";
 });
-customersBtn.addEventListener('mouseleave', function(){
-    imgBgcDIv.removeAttribute('style')
+customersBtn.addEventListener("mouseleave", function () {
+  imgBgcDIv.removeAttribute("style");
 });
